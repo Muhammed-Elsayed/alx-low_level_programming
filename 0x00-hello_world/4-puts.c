377769c916f21be91631ceb2a9acf4633ac6cb80
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-16 17:28:52 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main(){
 
-puts("\"Programming is like building a multilingual puzzle\n");
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+puts("\"Programming is like building a multilingual puzzle");
 
 return(0);
 }
