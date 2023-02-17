c69ea248037bb759eec8cb1b748d1625e5ba24dd
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-17 21:54:32 +0200

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - ENTRY point
+ *
+ * RETURN : always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
