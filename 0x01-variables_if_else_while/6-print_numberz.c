019408f97ef185116f350a567eb72bfa4a68ce03
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-17 22:13:27 +0200

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * print from 0 to 9
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
+
+
