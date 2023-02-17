516e6766ef8b71c0f034c6ef0ba2e503fae018b2
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-17 21:13:35 +0200

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch;
-	for (ch = 'a'; ch < 'z' ; ch++)
+	for (ch = 'a'; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
