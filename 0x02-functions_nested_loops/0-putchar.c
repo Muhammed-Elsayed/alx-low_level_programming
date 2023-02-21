b80f38dd47215ce433edcb6c759058f5ed39515e
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-22 01:51:42 +0200

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+
+
 int main(void)
 {
 	_putchar('_');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char let;
+
+	for ( char let = 'a'; let < 'z'; let++)
+	{
+		_putchar(let);
+	}
+	_putchar("\n");
+
+
+
+}
