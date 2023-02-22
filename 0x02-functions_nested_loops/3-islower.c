4a401e009b70da7b55888369d297b8edd005c2af
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-22 03:05:53 +0200

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 
 /**
- * int _islower - check if its lower case or not.
+ *_islower - check if its lower case or not.
  *
  * Return: 1 or 0.
  */
 
-int _islower(int c){
+int _islower(int c)
+{
 
 	return (c >= 97 && c <= 122);
 }
