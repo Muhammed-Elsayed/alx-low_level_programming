8aec3faca223cfda735009de2cf8d646ed6cf901
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-22 05:06:20 +0200

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,12 @@
 int print_last_digit(int number)
 {
 	int a;
-
-	if (number < 0)
-		number = -number;
-
 	a = number % 10;
+	
+	if (a < 0)
+		a = -a;
+
 
 	_putchar(a + '0');
 	return (a);
-
-	return (0);
-
 }
