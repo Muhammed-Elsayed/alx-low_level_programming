3ecc506a506df913b559ffc7c79b2d557244a806
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-02-28 04:11:56 +0200

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 	{
 	}
-	
+
 	if (len % 2 == 0)
 	{
 		n = len / 2;
@@ -24,7 +24,7 @@ void puts_half(char *str)
 		}
 	}
 
-	else if ( len % 2 != 0)
+	else if (len % 2 != 0)
 	{
 		n = (len - 1) / 2;
 		for (; n < (len - 1); n++)
@@ -32,5 +32,5 @@ void puts_half(char *str)
 			_putchar(str[n + 1]);
 		}
 	}
-	_putchar('\n'); 	
+	_putchar('\n');
 }
