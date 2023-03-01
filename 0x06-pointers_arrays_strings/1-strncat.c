bbaf0e52011448207630deaf81af5808a4d41263
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-01 20:38:54 +0200

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ *_strncat - function that concatenates two strings .
+ *@dest: ptr .
+ *@src: ptr .
+ *@n: int
+ *Return: pointer to a string.
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest = 0, len_src = 0, i;
+
+	while (dest[len_dest] != '\0')
+		len_dest++;
+
+	while (src[len_src] != '\0')
+		len_src++;
+
+
+	dest[len_dest] = ' ';
+	dest += len_dest;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	dest -= len_dest;
+	return (dest);
+}
