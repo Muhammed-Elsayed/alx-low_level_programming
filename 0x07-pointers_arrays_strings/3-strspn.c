79a71d69cdfeec58ed2168d8a3c24dc2b3203557
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-07 02:36:39 +0200

    5

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,10 +12,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
+	int the_accept = 0;
 
 	while (*s != '\0')
 	{
-		
+		accept -= the_accept;
 		while ( *accept != '\0')
 		{
 			if ( *s == *accept)
@@ -27,6 +28,7 @@ unsigned int _strspn(char *s, char *accept)
 			else
 			{
 				accept++;
+				the_accept++;
 				if (*accept == '\0')
 					return (i);
 			}
