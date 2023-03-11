8122c640f6a2f0614dd841249f2f31cef4715993
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-11 03:52:13 +0200

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
- *main - a program that adds positive numbers.
- *@argc: number of command line argument passed by the user .
- *@argv: array of pointer characters including th name of the program .
- *Return:  0 or 1 .
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
-
-
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int sum = 0;
+	int i;
 
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
-		return (0);
+	printf("%d\n", 0);
+	return (0);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
-		{
-			sum += atoi(argv[i]);
-		}
-
-		else
-		{
-			printf("Error\n");
-			return (1);
-
-		}
+		int num = atoi(argv[i]);
+	if (num <= 0)
+	{
+		printf("Error\n");
+	return (1);
+	}
+	sum += num;
 	}
 
 	printf("%d\n", sum);
-
 	return (0);
 }
-
