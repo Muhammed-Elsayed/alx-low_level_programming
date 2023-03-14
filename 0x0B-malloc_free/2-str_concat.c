b594aa5ee26f328a7a9719a6d27bccc381586235
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-15 01:41:59 +0200

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,26 +13,25 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int i, j;
+	int i = 0, j = 0;
 	int len = 0;
 	int m, n;
 	
-	if (s1 == NULL)
-		*s1 = '\0';
-
-	if (s2 == NULL)
-		*s2 = '\0';
-
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 != NULL)
 	{
+		for (; s1[i] != '\0'; i++)
+		{
+		}
 	}
-
-	for (j = 0; s2[j] != '\0'; j++)
+	
+	if (s2 != NULL)
 	{
+		for (; s2[j] != '\0'; j++)
+		{
+		}
 	}
-
+	
 	len += (i + j);
-
 
 	ptr = (char *) malloc(sizeof(char) * (len + 1));
 
