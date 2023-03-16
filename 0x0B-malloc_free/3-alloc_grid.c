9fe592f3baa73eb346f97b48ecdbd9fb8522b45b
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-16 03:28:06 +0200

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,11 +14,19 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	grid = (int **) malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+			return (NULL);
 
 		for (j = 0; j < width; j++)
 		{
