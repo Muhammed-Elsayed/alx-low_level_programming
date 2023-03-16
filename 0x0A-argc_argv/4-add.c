c8d64d3c9d356ef22133a1814a511e456320ea63
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-16 04:39:49 +0200

    final commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
@@ -22,17 +22,19 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		
-		if (isdigit(atoi(argv[i])))
-		{
-			sum += atoi(argv[i]);
-		}
 
-		else
+		for (j = 0; j < argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (isdigit(argv[i]) > 0)
+			{
+				sum += atoi(argv[i]);
+			}
 
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 
