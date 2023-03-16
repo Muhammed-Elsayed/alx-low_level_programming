0cbcff12b9508809e21c39135b9d4243a0aa0906
Mohamed Elsayed <70416898+Muhammed-Elsayed@users.noreply.github.com>
2023-03-16 04:35:45 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- *main - a program that adds positive numbers.
- *@argc: number of command line argument passed by the user .
- *@argv: array of pointer characters including th name of the program .
- *Return:  0 or 1 .
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
-
-
-
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int num, digit, sum = 0;
 
-	if (argc == 1)
+	for (num = 1; num < argc; num++)
 	{
-		printf("%d\n", 0);
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		
-		if (isdigit(atoi(argv[i])))
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			sum += atoi(argv[i]);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
-		else
-		{
-			printf("Error\n");
-			return (1);
-
-		}
+		sum += atoi(argv[num]);
 	}
 
 	printf("%d\n", sum);
