7ea51ef0d5237aec129905b227ad3eb7c4d2d6e9
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-19 02:16:37 +0200

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,10 +22,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (s1_len = 0; s2[s1_len] != '\0'; s1_len++)
-	{		
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+	{
 	};
-	
+
 	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
 	{
 	};
@@ -33,18 +33,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= s2_len)
 		n = s2_len;
 
-	ptr = (char *)malloc (sizeof(s1) + n);
+	ptr = (char *) malloc(sizeof(s1) + n);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	
+
 	for (i = 0; i < s1_len; i++)
 		ptr[i] = s1[i];
 
 	for (j = 0; j < n; j++)
 		ptr[i + j] = s2[j];
-	
+
 	ptr[i + j] = '\0';
 	return (ptr);
 }
