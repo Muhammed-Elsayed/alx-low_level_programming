7142531c218f1e1835e46f55917f858d4af4986d
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-19 03:06:53 +0200

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = (unsigned int *) malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+
+
+
+
+}
