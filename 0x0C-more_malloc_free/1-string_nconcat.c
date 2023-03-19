25b044cee60c09c5da4e957dd6a78f96ed2e19e7
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-19 02:03:44 +0200

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *string_nconcat - concatenating string and return a ptr
+ *@s1: the first str .
+ *@s2: the second str .
+ *@n: int .
+ *Return: ptr to string .
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len, s2_len;
+	char *ptr;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (s1_len = 0; s2[s1_len] != '\0'; s1_len++)
+	{		
+	};
+	
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+	{
+	};
+
+	if (n >= s2_len)
+		n = s2_len;
+
+	ptr = (char *)malloc (sizeof(s1) + n);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	
+	for (i = 0; i < s1_len; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		ptr[i + j] = s2[j];
+	
+	ptr[i + j] = '\0';
+	return (ptr);
+}
