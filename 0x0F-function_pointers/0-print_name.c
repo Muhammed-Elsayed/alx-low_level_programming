f149fc36d8b23b826f870348c0b8fc865826235d
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-23 03:16:43 +0200

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *print_name -  function that prints a name.
+ *@name: ptr to char .
+ *@f: ptr to function .
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
