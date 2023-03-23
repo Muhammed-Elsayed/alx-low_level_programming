0d3bdbb8acf025e8219a37df6ad151cfbfcf8b50
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-23 03:47:33 +0200

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	
-	if (action == NULL)
+
+	if (action == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-			action(array[i]);
+		action(array[i]);
 	}
 }
