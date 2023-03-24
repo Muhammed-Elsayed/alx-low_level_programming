cc108fba41376fcd98642cae13278c3b0de19963
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-03-24 04:08:41 +0200

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(list, char *));
+		if (separator == NULL)
+			printf("nil");
+		else
+			printf("%s", va_arg(list, char *));
+
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
