7854cc0a253c580730faff3a9435f402dbccd486
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-04-28 04:28:29 +0300

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    format db 'Hello, Holberton!', 10, 0 ; 10 is the ASCII code for a new line character
-
-section .text
-    global main
-
+          global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    ; call printf with the format string
-    mov rdi, format
-    xor esi, esi ; set the second argument to 0 (no arguments to substitute)
-    mov eax, 0 ; set the return value to 0
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
