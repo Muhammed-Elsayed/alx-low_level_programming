84ec97a7c89655f519dc307347f685a19fc30bb1
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-07 22:40:12 +0300

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *key_index - getting the index of the key
+ *@key: the key.
+ *@size: the size of the table.
+ *Return: the index.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int index = dibj2(key) % size;
+
+	return (index);
+}
