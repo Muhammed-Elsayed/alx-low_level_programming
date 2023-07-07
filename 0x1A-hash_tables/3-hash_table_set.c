f9023cca8a60db19ba6372b186122172704ce917
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-08 01:13:27 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,16 +41,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (0);
 	}
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
+		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
