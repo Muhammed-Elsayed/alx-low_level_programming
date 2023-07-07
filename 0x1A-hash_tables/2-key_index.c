7d3cad3b13dfd75d6013fdea273ed45adb748431
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-07 22:42:34 +0300

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,7 +11,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned int index = dibj2(key) % size;
+	unsigned int index = hash_djb2(key) % size;
 
 	return (index);
 }
