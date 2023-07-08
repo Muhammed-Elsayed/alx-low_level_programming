567b14957f329e6f25ec61bf0d551d440a3941ea
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-08 15:29:30 +0300

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - printing the hash table
+ *@ht: ptr to the hash table.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node;
+	int first_node = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (first_node)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first_node = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
