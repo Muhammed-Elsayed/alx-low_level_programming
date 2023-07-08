073cbab91cb9c5c952bbcf496b7cbfa501d2fc23
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-07-08 15:31:32 +0300

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,6 +11,11 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	int first_node = 0;
 
+	if (ht == NULL)
+	{
+		return;
+	}
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
